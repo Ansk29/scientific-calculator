b19282ed26936e5eba8886b7407885cb53c99d1e
anshul kakirde <anshul.kakirde@gmail.com>
2025-02-24 22:21:03 +0530

    jenkins configuration

---
+++ build/CMakeFiles/scientific_calculator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/scientific_calculator.dir/calculator.cpp.o"
+  "CMakeFiles/scientific_calculator.dir/calculator.cpp.o.d"
+  "CMakeFiles/scientific_calculator.dir/main.cpp.o"
+  "CMakeFiles/scientific_calculator.dir/main.cpp.o.d"
+  "scientific_calculator"
+  "scientific_calculator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scientific_calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    std::cout << "Scientific Calculator Running..." << std::endl;
+    return 0;
+}
+
